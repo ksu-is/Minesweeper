f76f74c1a535def2e7492c4f28444a9075b69f13
TraceL0L <89104234+TraceL0L@users.noreply.github.com>
2021-11-20 01:34:56 -0500

    Update MinesweeperProject.py
    Attempting to make the grid in which the game will operate

--- MinesweeperProject.py
+++ MinesweeperProject.py
@@ -1,8 +1,19 @@
-# MineSweeper Layout 
-def print_mine_layout():
+# Creating MineSweeper Grid Layout 
+def mine_grid():
     global mine_values
     global n 
 
     print()
     print("MineSweeper")
 
+    st = "   "
+    for i in range(n):
+        st = st + "     " + str(i + 1)
+    print(st)   
+ 
+    for r in range(n):
+        st = "     "
+        if r == 0:
+            for col in range(n):
+                st = st + "______" 
+            print(st)
