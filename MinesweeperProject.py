efc35b24d757f21378ae5d260089bf6464e6e587
TraceL0L <89104234+TraceL0L@users.noreply.github.com>
2021-11-20 18:53:52 -0500

    Update MinesweeperProject.py
    creating the mine positioning in the board

--- MinesweeperProject.py
+++ MinesweeperProject.py
@@ -1,6 +1,17 @@
 # MineSweeper Game
-
+from random import randint
 # Making mines 
 mines = set()
 # Making board 
 board = [["-" for x in range(10)] for x in range(10)]
+
+#  randomly positioning the mines
+def make_mines(mines):
+    
+    a = (randint(0,10),randint(0,10))
+    b = (randint(0,10),randint(0,10))
+    c = (randint(0,10),randint(0,10))
+    
+    mines.add(a)
+    mines.add(b)
+    mines.add(c)
