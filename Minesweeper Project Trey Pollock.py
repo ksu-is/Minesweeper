964dc70885f111d8767d79010281bb4c27373e74
TraceL0L <89104234+TraceL0L@users.noreply.github.com>
2021-11-20 05:38:56 -0500

    Restarted to try and simplify
    Created more simple board and mines

--- Minesweeper Project Trey Pollock.py
+++ Minesweeper Project Trey Pollock.py
@@ -1,8 +1,1 @@
-# MineSweeper Layout 
-def print_mine_layout():
-    global mine_values
-    global n 
-
-    print()
-    print("MineSweeper")
 
--- MinesweeperProject.py
+++ MinesweeperProject.py
@@ -1,19 +1,6 @@
-# Creating MineSweeper Grid Layout 
-def mine_grid():
-    global mine_values
-    global n 
+# MineSweeper Game
 
-    print()
-    print("MineSweeper")
-
-    st = "   "
-    for i in range(n):
-        st = st + "     " + str(i + 1)
-    print(st)   
- 
-    for r in range(n):
-        st = "     "
-        if r == 0:
-            for col in range(n):
-                st = st + "______" 
-            print(st)
+# Making mines 
+mines = set()
+# Making board 
+board = [["-" for x in range(10)] for x in range(10)]
