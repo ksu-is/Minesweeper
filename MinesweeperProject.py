a58c3aebde01783914ff61b10dcb0dfe5f33fee4
TraceL0L <89104234+TraceL0L@users.noreply.github.com>
2021-11-20 19:09:03 -0500

    Update MinesweeperProject.py
    Starting to implement the code to find the mines around the selected mine

--- MinesweeperProject.py
+++ MinesweeperProject.py
@@ -7,7 +7,7 @@ board = [["-" for x in range(10)] for x in range(10)]
 
 #  randomly positioning the mines
 def make_mines(mines):
-    
+
     a = (randint(0,10),randint(0,10))
     b = (randint(0,10),randint(0,10))
     c = (randint(0,10),randint(0,10))
@@ -15,3 +15,19 @@ def make_mines(mines):
     mines.add(a)
     mines.add(b)
     mines.add(c)
+
+# Reavels the mines touching the accessed space's number
+def border_nums(board, x, y):
+    numMines = 0
+    
+    # Top-left Space
+    if (x > 0 and y > 0):
+        c = (x-1, y-1)
+        if c in mines:
+            numMines += 1
+
+    # Top Space
+    if (x > 0):
+        c = (x-1, y)
+        if c in mines:
+            numMines += 1
