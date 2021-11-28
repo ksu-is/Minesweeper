e2f1729828286e9b611f770a11ef9751dd9569c9
TraceL0L <89104234+TraceL0L@users.noreply.github.com>
2021-11-28 18:25:26 -0500

    Update MinesweeperProject.py
    Had to start over again.  Kept the import random function. Then using the random function to place a bomb labeled "X" into the board.

--- MinesweeperProject.py
+++ MinesweeperProject.py
@@ -1,66 +1,43 @@
 # MineSweeper Game
-from random import randint
-# Making mines 
-mines = set()
-# Making board 
-board = [["-" for x in range(10)] for x in range(10)]
+import random
+#  Randomly positioning the mines
+def MakeMines(a,z):
+    disp = [[0 for row in range(a)] for column in range(a)]
+    for num in range(z):
+        x = random.randint(0,a-1)
+        y = random.randint(0,a-1)
+        disp[y][x] = 'X'
+        if (x >=0 and x <= a-2) and (y >= 0 and y <= a-1):
+                # Center-Right
+            if disp[y][x+1] != 'X':
+                disp[y][x+1] += 1 
+                # Center-Left
+        if (x >=1 and x <= a-1) and (y >= 0 and y <= a-1):
+            if disp[y][x-1] != 'X':
+                disp[y][x-1] += 1 
+                # Top-Left
+        if (x >= 1 and x <= a-1) and (y >= 1 and y <= a-1):
+            if disp[y-1][x-1] != 'X':
+                disp[y-1][x-1] += 1 
+                # Top-Right
+        if (x >= 0 and x <= a-2) and (y >= 1 and y <= a-1):
+            if disp[y-1][x+1] != 'X':
+                disp[y-1][x+1] += 1 
+                # Top-Center
+        if (x >= 0 and x <= a-1) and (y >= 1 and y <= a-1):
+            if disp[y-1][x] != 'X':
+                disp[y-1][x] += 1 
+                # Bottom-Right
+        if (x >=0 and x <= a-2) and (y >= 0 and y <= a-2):
+            if disp[y+1][x+1] != 'X':
+                disp[y+1][x+1] += 1 
+                # Bottom-Left
+        if (x >= 1 and x <= a-1) and (y >= 0 and y <= a-2):
+            if disp[y+1][x-1] != 'X':
+                disp[y+1][x-1] += 1 
+                # Bottom-Center
+        if (x >= 0 and x <= a-1) and (y >= 0 and y <= a-2):
+            if disp[y+1][x] != 'X':
+                disp[y+1][x] += 1 
 
-#  randomly positioning the mines
-def make_mines(mines):
-
-    a = (randint(0,10),randint(0,10))
-    b = (randint(0,10),randint(0,10))
-    c = (randint(0,10),randint(0,10))
-    
-    mines.add(a)
-    mines.add(b)
-    mines.add(c)
-
-# Reavels the mines touching the accessed space's number
-def border_nums(board, x, y):
-    numMine = 0
-    
-    # Top-left Space
-    if (x > 0 and y > 0):
-        c = (x-1, y-1)
-        if c in mines:
-            numMine += 1
-
-    # Top Space
-    if (x > 0):
-        c = (x-1, y)
-        if c in mines:
-            numMine += 1
-
-     # Top-right Space
-    if (x > 0 and y < 4):
-        c = (x-1, y+1)
-        if c in mines:
-            numMine += 1
-    # Left
-    if (y > 0):
-        c = (x, y-1)
-        if c in mines:
-            numMine += 1
-    # Right
-    if (y < 4):
-        c = (x, y+1)
-        if c in mines:
-            numMine += 1
-    # Bottom-left
-    if (x < 4 and y > 0):
-        c = (x+1, y-1)
-        if c in mines:
-            numMine += 1
-    # Bottom
-    if (x < 4):
-        c = (x+1, y)
-        if c in mines:
-            numMine += 1
-    # Bottom-right
-    if (x < 4 and y < 4):
-        c = (x+1, y+1)
-        if c in mines:
-            numMine += 1
-
-   
+    return disp
